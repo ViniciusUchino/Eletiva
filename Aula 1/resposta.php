e55d6f9a6b97d411271fd3392abf20f2ae50fdcb
Vinicius Uchino <viniciusuchteste@gmail.com>
2024-08-20 19:57:44 -0300

    aula 2

---
+++ Aula 1/resposta.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $valor = (int) $_POST['valor'];
+        $valor = (float) $_POST['valor'];
+        $valor = (string) $_POST['valor'];
+        //$valor = (bool) $_POST['valor'];
+        echo "Valor informado: $valor";
+    ?>
+</body>
+</html>
