50a0c305ac08fc1e972fee51ca1544f3224e04bd
Vinicius Uchino <viniciusuchteste@gmail.com>
2024-08-27 19:39:02 -0300

    dia 27-08

--- Lista2/exer7resp.php
+++ Lista2/exer7resp.php
@@ -4,22 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Resposta exercicio 4</title>
+    <title>Resposta exercicio 7</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Resposta do exercício 5</h1>
+    <h1>Resposta do exercício 7</h1>
     <?php
         if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             try {
-                $valor1 = (int) $_POST['valor1'] ?? 0;
-                $valor2 = (int) $_POST['valor2'] ?? 0;
-                $valor3 = (int) $_POST['valor3'] ?? 0;
+                $temp = (int) $_POST['temp'] ?? 0;
                 // +   -   *   /   %   ++   --  **
                
-                $resultado = ($valor1 +  $valor2 + $valor3) / 3;
-                echo "<p>Média: $resultado </p>";
+                $resultado = ($temp - 32) / 1.8;
+                echo "<p>Temperatura em C°: $resultado </p>";
                 
             } catch (Exception $e) {
                 echo "Erro! " . $e->getMessage();
