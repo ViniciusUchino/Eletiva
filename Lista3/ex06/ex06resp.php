5851b6a5f2d32f43bd20900d9defaa2693dbfedf
Vinicius Uchino <viniciusuchteste@gmail.com>
2024-09-10 18:51:09 -0300

    Exercicio 6 ao 10

---
+++ Lista3/ex06/ex06resp.php
@@ -0,0 +1,28 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercicio 06</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+	<?php
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+			try{
+				$n1 = (int)$_POST["n1"];	
+				
+				for($i = 1; $i <= $n1; $i++){
+					echo"$i ";
+				}
+					
+			}catch(Exception $e){
+				echo"Erro".$e->getMessage();
+			}
+		}
+		
+	?>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
---
+++ Lista3/ex10/ex10.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ex10</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">	
+</head>
+<body>
+	<form action="ex10resp.php" method="post">
+		<div class="row m-5">
+			<div class="col">
+				<label class="form-label" for="n1">Insira um número para descobrir sua tabuada</label>
+				<input type="text" class="form-control" name="n1" id="n1">
+			</div>
+		</div>
+		<div class="row m-5">
+			<div class="col">
+				<button type="submit" class="btn btn-success">Enviar</button>
+			</div>
+		</div>
+	</form>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
